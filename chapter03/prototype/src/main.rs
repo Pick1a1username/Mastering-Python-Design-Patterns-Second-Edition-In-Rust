df41ada080f16a2a0ec177db10b80e930492f34e
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2019-05-18 18:41:40 +0900

    prototype updated, work in progress

--- chapter03/prototype/src/main.rs
+++ chapter03/prototype/src/main.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 // Since there's no feature like '**kwargs' of Python in Rust,
 // and additional fields cannot be add to defined struct,
 // HashMap is used instead in this example.
+#[derive(Clone)]
 struct Website {
     info: HashMap<String, String>,
 }
@@ -27,18 +28,72 @@ impl Website {
 
     fn get_info(&self) -> String {
         let mut summary = vec![format!("Website \"{}\"", self.info["name"])];
-        let mut infos = self.info.clone();
 
-        String::from("test")
+        let mut infos = self.info.clone();
+        sort_string_only_hashmap(&mut infos);
+
+        for (key, value) in infos {
+            if key == String::from("name") {
+                continue
+            }
+            summary.push(format!("{}: {}\n", key, value));
+        }
+
+        let mut summary_for_print = String::new();
+
+        for entry in summary {
+            summary_for_print.push_str(&entry);
+        }
+
+        summary_for_print
     }
 }
 
+struct Prototype {
+    objects: HashMap<String, Website>,
+}
+
+impl Prototype {
+    fn new() -> Prototype {
+        Prototype {
+            objects: HashMap::new(),
+        }
+    }
+
+    fn register(&mut self, identifier: String, obj: Website) {
+        self.objects.insert(identifier, obj);
+    }
+
+    fn unregister(&mut self, identifier: String) {
+        self.objects.remove(&identifier);
+    }
+
+    fn clone(&mut self, identifier: String, new_infos: HashMap<String, String>) -> Result<Website, String>  {
+        let found = self.objects.get(&identifier);
+
+        match found {
+            Some(current_obj) => {
+                let mut new_obj = current_obj.clone();
+
+                // Todo: If the key already exists, it should be overwritten.
+                for (new_key, new_value) in new_infos {
+                    new_obj.info.insert(new_key.clone(), new_value.clone());
+                }
+
+                Ok(new_obj)
+            },
+            None => Err(format!("Incorrect object identifier: {}", identifier)),
+        }
+    }
+}
+
+
 fn sort_string_only_hashmap(hashmap: &mut HashMap<String, String>) {
     // Get keys
-    let mut keys: Vec<&String> = Vec::new();
+    let mut keys: Vec<String> = Vec::new();
     
-    for ref key in hashmap.keys() {
-        keys.push(key);
+    for key in hashmap.keys() {
+        keys.push(key.clone());
     }
 
     // Sort key vector
@@ -46,14 +101,31 @@ fn sort_string_only_hashmap(hashmap: &mut HashMap<String, String>) {
  
     // Recreate hashmap
     let mut sorted_hashmap: HashMap<String, String> = HashMap::new();
-    for ref key in keys {
-        sorted_hashmap.insert(key, hashmap[key]);
+    for key in keys {
+        sorted_hashmap.insert(key.clone(), hashmap[&key].clone());
     }
 
-
     // Set the variable to the recreated hashmap
+    *hashmap = sorted_hashmap;
 }
 
 fn main() {
-    println!("Hello, world!");
+    // Since HashMap can have the type of value specified in advance,
+    // keywords will be set as String comma-breaked.
+    let keywords: String = String::from("python,data,apis,automation");
+    let mut keywords_mapped: HashMap<String, String> = HashMap::new();
+    keywords_mapped.insert(String::from("keywords"), keywords);
+
+    let site1 = Website::new("ContentGardening".to_string(),
+        "contentgardening.com".to_string(),
+        "Automation and data-driven apps".to_string(),
+        "Kamon Ayeva".to_string(),
+        keywords_mapped
+    );
+
+    let mut prototype = Prototype::new();
+    let identifier = "ka-cg-1".to_string();
+    prototype.register(identifier, site1);
+
+    // 
 }
