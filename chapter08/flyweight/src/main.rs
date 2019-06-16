fbd868de09e03d115f21acebfdaa69c748594ad3
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2019-06-16 18:38:28 +0900

    20190616_2

--- chapter08/flyweight/src/main.rs
+++ chapter08/flyweight/src/main.rs
@@ -20,14 +20,30 @@ struct Car {
 }
 
 impl Car {
-    fn new(car_type: CarType) -> Rc<Car> {
+    fn new(&self, car_type: CarType) -> Rc<Car> {
+        match self.checker(&car_type) {
+            true => {
+               Rc::clone(self.pool.borrow().get(&car_type).unwrap())
+            },
+            false => {
+                let new_car = Rc::new(Car { pool: Rc::clone(&self.pool) });
+                self.pool.borrow_mut().insert(
+                    car_type,
+                    Rc::clone(&new_car)
+            );
+                Rc::clone(&new_car)
+            }
+        }
+    }
+
+    fn init() -> Rc<Car> {
         let mut new_pool: HashMap<CarType, Rc<Car>> = HashMap::new();
         let new_car = Rc::new(Car { pool: Rc::new(RefCell::new(new_pool)) });
-        new_car.pool.borrow_mut().insert(
-            car_type,
-            Rc::clone(&new_car)
-        );
         Rc::clone(&new_car)
+    }
+
+    fn checker(&self, car_type: &CarType) -> bool {
+        self.pool.borrow().contains_key(&car_type)
     }
 }
 
@@ -40,9 +56,11 @@ impl fmt::Pointer for Car {
 }
 
 fn main() {
-    let car_a = Car::new(CarType::Suv);
-    let car_b = Car::new(CarType::Suv);
-    let car_c = Car::new(CarType::Compact);
+    let car_generator = Car::init();
+
+    let car_a = car_generator.new(CarType::Suv);
+    let car_b = car_generator.new(CarType::Suv);
+    let car_c = car_generator.new(CarType::Compact);
 
     println!("{:p}", car_a);
     println!("{:p}", car_b);
